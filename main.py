477804b1c9587726dc8a225793c3d4efef9e7666
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-10-29 16:27:33 +0100

    python changes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-# Machine learning program to work out the possible stock prices
+import random
+from typing import Mapping
+import requests
+from bs4 import BeautifulSoup
 
-import numpy as np
-import pandas as pd
+url = 'https://www.imdb.com/chart/top'
 
-tesla = pd.read_csv('TSLA.csv')
-
-
+def main():
+    response = requests.get(url)
+    html = response.text
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    movietags = soup.select('td.titleColumn')
+    movietag0 = movietags[0]
+    moviesplit = movietag0.text.split()
+    
+    print(moviesplit)
+    
+    
+if __name__ == '__main__':
+    main()
